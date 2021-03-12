754bfe27dd1bc1585fb29e7e627481dbdbe3c9cb
Mahitha A <mahitha@everest.engineering>
2021-03-12 13:29:35 +0530

    Create a book

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "/books", to: "books#index"
+  get "/books/new", to: "books#new"
+  post "/books", to: "books#create"
 end
