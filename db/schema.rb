f15f503f4c97ab741f5603ff1c4aa269e374200a
Mahitha A <mahitha@everest.engineering>
2021-03-15 09:59:13 +0530

    Create user model and update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_101557) do
+ActiveRecord::Schema.define(version: 2021_03_15_042702) do
 
   create_table "books", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "author"
     t.integer "edition"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
