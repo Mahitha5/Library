3d2170db1cde90ea92d0d29cd22e9b1ee1fbf0cd
Mahitha A <mahitha@everest.engineering>
2021-03-18 15:17:16 +0530

    Add unit tests for book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should return false if title is not present" do
+    @book = Book.new(title: "", author: "Me")
+    assert_not @book.valid?
+  end
+
+  test "Should return false if author is not present" do
+    @book = Book.new(title: "Learn Ruby on Rails", author: "")
+    assert_not @book.valid?
+  end
+
+  test "Should return true when book object is valid" do
+    @book = Book.new(title: "Learn RoR", author: "Someone")
+    assert @book.valid?
+  end
 end
