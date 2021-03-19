d9f2953c091b3da31131952152969c8a9ccf3eef
Mahitha A <mahitha@everest.engineering>
2021-03-19 17:23:18 +0530

    Add functional tests for book controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should get index" do
+    get books_url
+    assert_response :success
+  end
+
+  test "Should create new book" do
+    post "/books", params: { book: { title: "Learn RoR", author: "Someone", edition: 2 }}
+    assert_redirected_to "/books"
+  end
+
+  test "Should render new on create fail" do
+    post "/books", params: { book: { title: "Learn RoR", author: "", edition: 2 }}
+    assert_template :new
+  end
 end
